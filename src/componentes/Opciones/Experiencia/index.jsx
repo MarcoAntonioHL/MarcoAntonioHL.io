1bfaa2e88de2a523fe7c766dcce9c1896f41f4b0
Marco <marco.hl9antonio@gmail.com>
2025-07-08 11:58:38 -0500

    actualización de CV 080725

--- src/componentes/Opciones/Experiencia/index.jsx
+++ src/componentes/Opciones/Experiencia/index.jsx
@@ -20,7 +20,15 @@ const historial=[
     fechaFin:'Febrero 2025',
     tipoContrato:'Contrato de practicas profesionales',
     company:'Electro Sur Este S.A.A',
-    description:'Empresa donde actualmente laboro desarrollando aplicaciones web, realizando mantenimiento de módulos de escritorio del sistema SIELSE en .Net Framework 4.8. -Utilizando herramientas Front-end como React 18.2, TypeScript 5.4, Bootstrap 5.3 y herramientas Back-end como ASP.Net core API ,SQLserver, Git, GitHub, Swagger, Postman. '
+    description:'Empresa de comercialización de energía eléctrica en la cual desarrollé módulos web, mantenimiento de módulos de escritorio del sistema SIELSE administrativo en .Net Framework 4.8. -Utilizando herramientas Front-end como React 18.2, TypeScript 5.4, Bootstrap 5.3 y herramientas Back-end como ASP.Net core API ,SQLserver, Git, GitHub, Swagger, Postman. '
+
+  },
+  {
+    fechaInicio:'marzo 2025',
+    fechaFin:'julio 2025',
+    tipoContrato:'RRHH',
+    company:'Q-Total S.A.C',
+    description:'Empresa consultora en gestión por procesos. Participé en la elaboración y/o automatización de procesos, siguiendo la metodología AS-IS, TO-BE y TO-DO, utilizando herramientas BPMN Bizagi, Camunda'
 
   },
 ]
